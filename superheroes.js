c6918a17a10efb722df7cbbbaf6e875ba99b7766
thordur <thordur921@gmail.com>
2022-02-06 17:12:39 +0100

    javascript working

--- superheroes.js
+++ superheroes.js
@@ -498,9 +498,28 @@ const superheroes = [
   },
 ];
 
-alert(
-  "1. Checking if JS file is linked up correctly. \n2. Always open the inspector/console when woorking with the JS \n3. Write your code in the end of the js-file \n4. Don't use alert boxes for UI... \n5. Delete this message or comment it out"
-);
-console.table(superheroes);
+
 
 /* write your code here */
+
+
+superheroes.forEach(showHero);
+
+function showHero(hero) {
+  const template = document.querySelector("template").content;
+  const clone = template.cloneNode(true);
+
+  clone.querySelector("img").src = hero.img;
+  clone.querySelector("h2").textContent = `${hero.name} | ${hero.lifePercentage}hp`;
+  clone.querySelector("h3").textContent = `challenged by ${hero.primaryNemesis}`;
+
+  clone.querySelector("p").textContent = `${hero.dob.day} / ${hero.dob.month} / ${hero.dob.year}`;
+
+  hero.powers.forEach(function (power){
+    const li = document.createElement("li");
+    li.textContent = power;
+    clone.querySelector("ul").appendChild(li);
+  });
+
+  document.querySelector("main").appendChild(clone);
+}
